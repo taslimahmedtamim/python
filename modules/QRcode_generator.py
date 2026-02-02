9fcca667f0c1f452bb8d537f2f00264bbf69937b
Taslim Ahmed Tamim <taslimahmedtamim4u@gmail.com>
2026-02-02 22:01:02 +0600

    QR code generator

---
+++ modules/QRcode_generator.py
@@ -0,0 +1,11 @@
+# QR Code Generator
+
+# 1. in terminal run: pip install qrcode[pil]
+# 2. run this code
+# 3. check your folder for the generated qrcode.png file
+
+import qrcode
+
+url = input("Enter the URL to encode in QR Code: ").strip()
+myqr = qrcode.make(url)
+myqr.save("qrcode.png", scale = 8)
